4b4658f334c7343e786fcbb346b0b01d2556af6e
Astroglia <Astroglia@github.com>
2020-01-31 10:19:30 -0600

    generalized 2d input to allow any amount of radii to calculate

--- calculate.py
+++ calculate.py
@@ -2,20 +2,20 @@ from sympy.solvers import solve
 from sympy import Symbol
 
 
-def generate_x_y(offsets, r1, r2):
+def generate_x_y(offsets, radii):
     if offsets is None:
         offsets = [ 0, 0, 0, 0 ]
-    a = offsets[0]
-    b = offsets[1]
-    c = offsets[2]
-    d = offsets[3]
     # implement:
     # https://www.wolframalpha.com/input/?i=%28x-a%29%5E2+%2B+%28y-b%29%5E2+%3D+r%5E2%2C+%28x-c%29%5E2+%2B+%28y-d%29%5E2+%3D+m%5E2%2C+solve+for+x%2C+y
     # (x - a)^2 + (y - b) ^2 = (r1)^2 ; (x - c)^2 + (y - d)^2 = (r2)^2
+    equations = []
     x = Symbol('x')
     y = Symbol('y')
-    solution = solve( [(x-a)**2 + (y - b)**2 - r1**2, (x-c)**2 + (y - d)**2 - r2**2 ], [x,y] )
-    for i in solution:
-        for x in i:
-            print( float(x))
-    #print(solution)
+    oi = 0
+    for i in range( len(radii) ):
+        curr_eq = (x - offsets[oi])**2 + (y - offsets[oi + 1])**2 - radii[i]**2
+        equations.append(curr_eq)
+        oi = oi + 2
+
+    solution = solve( equations, [x, y] )
+    return solution
--- mesh.py
+++ mesh.py
@@ -12,4 +12,5 @@ coordinates = [ [0,0], [radius_map[0,1], 0], [ [None, None] ]*(radius_map.shape[
 #
 print(coordinates)
 
-calculate.generate_x_y([1,2,3,4], 3, 5 )
+solution = calculate.generate_x_y([1,2,2,3], [3, 2] )
+print(solution)
